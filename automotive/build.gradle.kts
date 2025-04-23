26b08097d5c860131cd8bc973ba6addc75d0f24e
Benoît Dardenne <benoit.dardenne@gmail.com>
2025-04-23 23:15:51 +0200

    Allow HTTP servers

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "be.bendardenne.jellyfin.aaos"
         minSdk = 29
         targetSdk = 34
-        versionCode = 11
+        versionCode = 12
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

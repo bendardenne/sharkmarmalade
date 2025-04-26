f8caa4e473bcccbb29db79642de4d33588a4c496
Benoît Dardenne <benoit.dardenne@gmail.com>
2025-04-26 20:05:59 +0200

    Update to dependencies

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 
 android {
     namespace = "be.bendardenne.jellyfin.aaos"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "be.bendardenne.jellyfin.aaos"
         minSdk = 29
         targetSdk = 34
-        versionCode = 12
+        versionCode = 17
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -38,22 +38,22 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.media3:media3-exoplayer:1.4.1")
-    implementation("androidx.media3:media3-session:1.4.1")
-    implementation("androidx.media3:media3-ui:1.4.1")
+    implementation("androidx.media3:media3-exoplayer:1.6.1")
+    implementation("androidx.media3:media3-session:1.6.1")
+    implementation("androidx.media3:media3-ui:1.6.1")
     implementation("androidx.concurrent:concurrent-futures-ktx:1.2.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jellyfin.sdk:jellyfin-core:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.dagger:hilt-android:2.51.1")
-    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials:1.5.0")
     kapt("com.google.dagger:hilt-compiler:2.51.1")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }

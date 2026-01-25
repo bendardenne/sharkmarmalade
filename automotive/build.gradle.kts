851b28b7ae853ebf8ee0a1fd9fd1c9b4fb4f6549
Benoît Dardenne <benoit.dardenne@gmail.com>
2026-01-25 15:41:25 +0100

    #11 Improve layout of login flow on smaller screens

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "be.bendardenne.jellyfin.aaos"
         minSdk = 29
         targetSdk = 34
-        versionCode = 26
+        versionCode = 27
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
